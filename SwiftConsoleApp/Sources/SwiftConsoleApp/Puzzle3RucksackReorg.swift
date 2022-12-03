de9eef06b9671d5ff075c20d6e580bee88056514
Orion Edwards <orion.edwards@gmail.com>
2022-12-04 12:16:42 +1300

    P1 should go before P2

--- SwiftConsoleApp/Sources/SwiftConsoleApp/Puzzle3RucksackReorg.swift
+++ SwiftConsoleApp/Sources/SwiftConsoleApp/Puzzle3RucksackReorg.swift
@@ -24,36 +24,6 @@ private func priority(character: Character) -> Int {
     }
 }
 
-struct Puzzle3RucksackReorgP2 {
-    private static func parseFile(fileName: String) throws -> [[Rucksack]] {
-        return try Puzzle3RucksackReorgP1
-            .parseFile(fileName: fileName)
-            .inGroupsOf(3)
-    }
-    
-    public static func run(fileName: String) throws {
-        let grandTotal = try parseFile(fileName: fileName)
-            .map { entrySet in
-                var common = [Character:Int]()
-                for rucksack in entrySet {
-                    for c in rucksack.contents.distinct() {
-                        common[c] = (common[c] ?? 0) + 1
-                    }
-                }
-                if let kv = common.first(where: { (k, v) in v == entrySet.count }) {
-//                    print("group: character=\(kv.key)")
-                    return priority(character: kv.key)
-                } else {
-                    print("group: no common character!")
-                    return 0 // no common item
-                }
-            }
-            .reduce(0, +)
-        
-        print("Grand Total=\(grandTotal)")
-    }
-}
-
 struct Puzzle3RucksackReorgP1 {
     fileprivate static func parseFile(fileName: String) throws -> [Rucksack] {
         return try linesInFile(fileName)
@@ -89,3 +59,35 @@ struct Puzzle3RucksackReorgP1 {
         print("Grand Total=\(grandTotal)")
     }
 }
+
+struct Puzzle3RucksackReorgP2 {
+    private static func parseFile(fileName: String) throws -> [[Rucksack]] {
+        return try Puzzle3RucksackReorgP1
+            .parseFile(fileName: fileName)
+            .inGroupsOf(3)
+    }
+    
+    public static func run(fileName: String) throws {
+        let grandTotal = try parseFile(fileName: fileName)
+            .map { entrySet in
+                var common = [Character:Int]()
+                for rucksack in entrySet {
+                    for c in rucksack.contents.distinct() {
+                        common[c] = (common[c] ?? 0) + 1
+                    }
+                }
+                if let kv = common.first(where: { (k, v) in v == entrySet.count }) {
+//                    print("group: character=\(kv.key)")
+                    return priority(character: kv.key)
+                } else {
+                    print("group: no common character!")
+                    return 0 // no common item
+                }
+            }
+            .reduce(0, +)
+        
+        print("Grand Total=\(grandTotal)")
+    }
+}
+
+
