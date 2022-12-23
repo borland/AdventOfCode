d1e534344431c4dbae395600da670fcdaf1ac7b0
Orion Edwards <orion.edwards@gmail.com>
2022-12-23 23:20:27 +1300

    Day 20 p2

--- SwiftConsoleApp/Sources/SwiftConsoleApp/Day20GrovePositioningSystem.swift
+++ SwiftConsoleApp/Sources/SwiftConsoleApp/Day20GrovePositioningSystem.swift
@@ -9,24 +9,23 @@ private func shift<T>(_ array: inout [T], index: Int, by: Int) {
     array.insert(item, at: insertionIdx)
 }
 
-private func mix(_ numbers: [Int]) -> [Int] {
-    typealias ListItem = (number: Int, originalIndex: Int)
-    
-    var listItems = numbers.enumerated().map { (idx, n) in ListItem(number: n, originalIndex: idx) }
-    
-    for i in listItems.indices {
-        guard let pos = listItems.firstIndex(where: { $0.originalIndex == i }) else {
-            fatalError("logic error, can't find an item with originalIndex \(i)")
-        }
-//        print("\(result[pos].number) moves from index \(pos) by \(result[pos].number)")
-        shift(&listItems, index: pos, by: listItems[pos].number)
-//        print(result)
-    }
-    
-    return listItems.map { $0.number }
-}
-
 struct Day20GrovePositioningSystemP1 {
+    private static func mix(_ numbers: [Int]) -> [Int] {
+        typealias ListItem = (number: Int, originalIndex: Int)
+        
+        var listItems = numbers.enumerated().map { (idx, n) in ListItem(number: n, originalIndex: idx) }
+        
+        for i in listItems.indices {
+            guard let pos = listItems.firstIndex(where: { $0.originalIndex == i }) else {
+                fatalError("logic error, can't find an item with originalIndex \(i)")
+            }
+    //        print("\(result[pos].number) moves from index \(pos) by \(result[pos].number)")
+            shift(&listItems, index: pos, by: listItems[pos].number)
+    //        print(result)
+        }
+        
+        return listItems.map { $0.number }
+    }
     
     static func run(fileName: String) throws {
         let numbers = try linesInFile(fileName).compactMap { Int($0) }
@@ -43,6 +42,48 @@ struct Day20GrovePositioningSystemP1 {
             let target = mixed[(x + offsetOfZeroValue) % mixed.count]
             print(target)
             sum += target
+        }
+        print("sum of targets = \(sum)")
+    }
+}
+
+struct Day20GrovePositioningSystemP2 {
+    typealias ListItem = (number: Int, originalIndex: Int)
+    
+    private static func mix(_ listItems: [ListItem]) -> [ListItem] {
+        var result = listItems
+        for i in result.indices {
+            guard let pos = result.firstIndex(where: { $0.originalIndex == i }) else {
+                fatalError("logic error, can't find an item with originalIndex \(i)")
+            }
+            shift(&result, index: pos, by: result[pos].number)
+        }
+        
+        return result
+    }
+    
+    static func run(fileName: String) throws {
+        let decryptionKey = 811589153
+        
+        let numbers = try linesInFile(fileName).compactMap { Int($0) }
+        var listItems = numbers.enumerated().map { (idx, n) in
+            ListItem(number: n * decryptionKey, originalIndex: idx)
+        }
+        
+        for _ in 0..<10 {
+            listItems = mix(listItems)
+//            print("after \(i+1) round of mixing:")
+//            print(listItems)
+        }
+        
+        guard let offsetOfZeroValue = listItems.firstIndex(where: { $0.number == 0 }) else { fatalError("can't find zero value") }
+//        print("offsetOfZeroValue = \(offsetOfZeroValue)")
+        
+        var sum = 0
+        for x in [1000, 2000, 3000] {
+            let target = listItems[(x + offsetOfZeroValue) % listItems.count]
+            print(target)
+            sum += target.number
         }
         print("sum of targets = \(sum)")
     }
--- SwiftConsoleApp/Sources/SwiftConsoleApp/main.swift
+++ SwiftConsoleApp/Sources/SwiftConsoleApp/main.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 do {
-    try Day20GrovePositioningSystemP1.run(fileName: "/Users/orion/Dev/aoc22/day20-input.txt")
+    try Day20GrovePositioningSystemP2.run(fileName: "/Users/orion/Dev/aoc22/day20-input.txt")
 
 } catch let err {
     print("Failed with error \(err)")
