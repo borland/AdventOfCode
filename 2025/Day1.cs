0f9f5f0b856e882f0fc9cd9204514c7ab3d0e845
Orion Edwards <orion.edwards@gmail.com>
2025-12-02 16:15:02 +1300

    Do it in steps

--- 2025/Day1.cs
+++ 2025/Day1.cs
@@ -16,12 +16,6 @@ public class Day1
         L82
         """;
 
-    enum Direction
-    {
-        L,
-        R
-    }
-
     public static void Run()
     {
         const int dialPositionCount = 100;
@@ -29,30 +23,46 @@ public class Day1
         int dialPosition = 50;
 
         var zeroCount = 0;
+        var zeroClickCount = 0;
         Console.WriteLine("The dial starts by pointing at {0}", dialPosition);
 
         foreach (var line in File.ReadAllLines("Day1Input.txt"))
+        //foreach (var line in ExampleInput.EnumerateLines())
         {
             var reader = new DelimitedLineReader(line);
             var leftOrRight = reader.ReadChar();
-            var amount = reader.ReadInt();
+            var originalAmount = reader.ReadInt();
+            var amount = originalAmount;
+            
+            // super brute force
             switch (leftOrRight)
             {
                 case 'L':
-                    dialPosition -= amount;
-                    while (dialPosition < 0) dialPosition += dialPositionCount;
+                    for (; amount > 0; --amount)
+                    {
+                        --dialPosition;
+                        if (dialPosition < 0) dialPosition += dialPositionCount;
+                        if (dialPosition == 0) ++zeroClickCount;
+                    }
+                   
                     break;
                 case 'R':
-                    dialPosition += amount;
-                    while (dialPosition >= dialPositionCount) dialPosition -= dialPositionCount;
+                    for (; amount > 0; --amount)
+                    {
+                        ++dialPosition;
+                        if (dialPosition >= dialPositionCount) dialPosition -= dialPositionCount;
+                        if (dialPosition == 0) ++zeroClickCount;
+                    }
+                   
                     break;
                 default:
                     throw new NotSupportedException(leftOrRight.ToString());
             }
-            Console.WriteLine("The dial is rotated {0}{1} to point at {2}.", leftOrRight, amount, dialPosition);
             if (dialPosition == 0) ++zeroCount;
+            Console.WriteLine("The dial is rotated {0}{1} to point at {2}.", leftOrRight, originalAmount, dialPosition);
         }
         
         Console.WriteLine("ZeroCount is {0}.", zeroCount);
+        Console.WriteLine("ZeroClickCount is {0}.", zeroClickCount);
     }
 }
