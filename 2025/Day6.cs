fc2cd0706fa598ffcd93ccb0761d7ab7b60fe482
Orion Edwards <orion.edwards@gmail.com>
2025-12-08 10:10:32 +1300

    Put it down

--- 2025/Day6.cs
+++ 2025/Day6.cs
@@ -48,31 +48,79 @@ public static class Day6
             Console.WriteLine(string.Join($" {OpChar(Operation)} ", Items) + $" = {Compute()}");
         }
     }
-    
+
     public static void Run()
     {
         // Parse
-        var problems = new List<MathProblem>();
-        // read the first line to work out the shape of the rest
+        var problems = ParseProblems(ExampleInput.EnumerateLines());
 
-        //using var e = ExampleInput.EnumerateLines().GetEnumerator();
-        using var e = File.ReadLines("Day6Input.txt").GetEnumerator();
-        if(!e.MoveNext()) throw new Exception("No first line in input?");
-        var firstLineReader = new DelimitedLineReader(e.Current);
-        while(firstLineReader.HasNonWhitespaceDataRemaining())
+        // now action them all
+        long grandTotal = 0;
+        foreach (var problem in problems)
         {
-            problems.Add(new MathProblem { Items = { firstLineReader.ReadSignedLong() } });
+            // problem.PrintWithResult();
+            var result = problem.Compute();
+            grandTotal += result;
         }
+
+        Console.WriteLine("Part 1 Grand total is {0}", grandTotal);
+        Console.WriteLine();
         
+        // Part 2: ridiculous BS
+        // TODO stop. I can't just take the `long` values I parsed before; whitespace is significant now, I need to go back and re-parse the file. Sigh.
+        foreach (var problem in problems.Select(p => ToCephalopodForm(p)))
+        {
+            problem.PrintWithResult();
+            var result = problem.Compute();
+            grandTotal += result;
+        }
+
+        Console.WriteLine("Part 2 Grand total is {0}", grandTotal);
+    }
+
+    // This is wrong. whitespace is significant now, I need to go back and re-parse the file. Sigh.
+    static MathProblem ToCephalopodForm(MathProblem problem)
+    {
+        var numbersAsListsOfChar = problem.Items.Select(i => i.ToString().AsEnumerable().ToList()).ToList();
+        var maxLength = numbersAsListsOfChar.Max(s => s.Count);
+
+        // now walk backwards, combining all the chars
+        var convertedProblem = new MathProblem { Operation = problem.Operation };
+        for (int i = maxLength; i >= 0; i--)
+        {
+            var tmp = new List<char>();
+            foreach (var n in numbersAsListsOfChar)
+            {
+                // if there's a digit in this column, add it to the buffer; else there's no digit so nothing to add
+                if (i < n.Count) tmp.Add(n[i]);
+            }
+            convertedProblem.Items.Add(int.Parse(new string(tmp.ToArray())));
+        }
+        return convertedProblem;
+    }
+
+    static List<MathProblem> ParseProblems(IEnumerable<string> lines)
+    {
+        var result = new List<MathProblem>();
+        // read the first line to work out the shape of the rest
+        
+        using var e = lines.GetEnumerator();
+        if (!e.MoveNext()) throw new Exception("No first line in input?");
+        var firstLineReader = new DelimitedLineReader(e.Current);
+        while (firstLineReader.HasNonWhitespaceDataRemaining())
+        {
+            result.Add(new MathProblem { Items = { firstLineReader.ReadSignedLong() } });
+        }
+
         // now read the rest of the lines
         while (e.MoveNext())
         {
             var reader = new DelimitedLineReader(e.Current);
 
-            for (int problemIdx = 0; problemIdx < problems.Count && reader.HasDataRemaining(); problemIdx++)
+            for (int problemIdx = 0; problemIdx < result.Count && reader.HasDataRemaining(); problemIdx++)
             {
-                var problem = problems[problemIdx];
-                
+                var problem = result[problemIdx];
+
                 // skip any leading whitespace. ReadSignedLong does this but PeekChar doesn't, let's help it out
                 reader.Scan(char.IsWhiteSpace);
                 var ch = reader.PeekChar();
@@ -92,17 +140,7 @@ public static class Day6
                 }
             }
         }
-        
-        // now action them all
-        long grandTotal = 0;
-        foreach (var problem in problems)
-        {
-            problem.PrintWithResult();
-            
-            var result = problem.Compute();
-            grandTotal += result;
-        }
 
-        Console.WriteLine("Grand total is {0}", grandTotal);
+        return result;
     }
 }
